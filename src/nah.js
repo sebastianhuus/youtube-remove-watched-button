75a41a9a3c62914ceda71cbd61da38bf1c391c27
Liam Ozog <liam.ozog@gmail.com>
2024-07-11 13:05:16 -0400

    move script out of window.onload so it works more reliably on FF

--- src/nah.js
+++ src/nah.js
@@ -1,33 +1,30 @@
-window.onload = function()
-{
-    // keep running so when new videos appear, ie. on page scroll, we add button to them as well
-    setInterval(function() {
-        addNahBtn("ytd-rich-grid-media #details");
-        addNahBtn("ytd-compact-video-renderer #dismissible .details");
-    }, 2000);
-    
-    const baseStyles = `
-    <style>
-        .nah-btn {
-            position: absolute;
-            top: 45px;
-            right: 0px;
-            padding: 7px 0 7px 7px;
-            background: none;
-            border: none;
-            cursor: pointer;
-            opacity: 0.5;
-        }
-        .nah-btn:hover {
-            opacity: 1;
-        }
-        .hide-popup {
-            opacity: 0;
-            display: none;
-        }
-    </style>`;
-    document.head.insertAdjacentHTML('beforeend', baseStyles);
-}
+// keep running so when new videos appear, ie. on page scroll, we add button to them as well
+setInterval(function() {
+    addNahBtn("ytd-rich-grid-media #details");
+    addNahBtn("ytd-compact-video-renderer #dismissible .details");
+}, 2000);
+
+const baseStyles = `
+<style>
+    .nah-btn {
+        position: absolute;
+        top: 45px;
+        right: 0px;
+        padding: 7px 0 7px 7px;
+        background: none;
+        border: none;
+        cursor: pointer;
+        opacity: 0.5;
+    }
+    .nah-btn:hover {
+        opacity: 1;
+    }
+    .hide-popup {
+        opacity: 0;
+        display: none;
+    }
+</style>`;
+document.head.insertAdjacentHTML('beforeend', baseStyles);
 
 function addNahBtn(videoBoxSelector)
 {
