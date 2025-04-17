fa6f8393c8f77b580bb069039032023b895d0f03
Sebastian Huus <54155988+sebastianhuus@users.noreply.github.com>
2025-04-17 14:47:19 +0200

    Refactor options.js to use async/await for storage operations and improve readability

--- src/options.js
+++ src/options.js
@@ -1,25 +1,17 @@
-// browser compatibility
-const storage =
-    typeof browser !== "undefined" ? browser.storage : chrome.storage;
+import { getFromStorage, setInStorage } from './storage_utilities.js';
 
-document.getElementById("save").addEventListener("click", () => {
+document.getElementById("save").addEventListener("click", async () => {
     const isDebuggingEnabled = document.getElementById("debug-mode").checked;
 
     // Save the checkbox value in storage
-    storage.sync.set(
-        { isDebuggingEnabled, shouldHideNahButton, shouldHideChannelButton },
-        () => {
-            document.getElementById("status").innerHTML = "Settings saved!";
-            setTimeout(() => {
-                document.getElementById("status").innerHTML = "";
-            }, 5000);
-        }
-    );
+    await setInStorage("isDebuggingEnabled", isDebuggingEnabled);
+    document.getElementById("status").innerHTML = "Settings saved!";
+    setTimeout(() => {
+        document.getElementById("status").innerHTML = "";
+    }, 5000);
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-    storage.sync.get("isDebuggingEnabled", (data) => {
-        document.getElementById("debug-mode").checked =
-            data.isDebuggingEnabled || false;
-    });
+document.addEventListener("DOMContentLoaded", async () => {
+    const isDebuggingEnabled = await getFromStorage("isDebuggingEnabled", false);
+    document.getElementById("debug-mode").checked = isDebuggingEnabled;
 });
